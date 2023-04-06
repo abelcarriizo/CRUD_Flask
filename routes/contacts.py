eba17dd227f9630e865102247b77a813c9ebe32d
Abel Carrizo <abelcarriizo@gmail.com>
2023-04-06 14:21:59 -0300

    añadiendo contactos

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from models.contact import Contact
+from utils.db import db
 
 contacts = Blueprint('contacts', __name__)
 
@@ -6,9 +8,17 @@ contacts = Blueprint('contacts', __name__)
 def home():
     return render_template('index.html')
 
-@contacts.route('/new')
+@contacts.route('/new', methods=['POST'])
 def add():
-    return 'saving contact'
+    fullname = request.form['fullname']
+    email = request.form['email']
+    phone = request.form['phone']
+
+    new_contact = Contact(fullname, email, phone)
+    db.session.add(new_contact)
+    db.session.commit()
+
+    return redirect('/')
 
 @contacts.route('/update')
 def update():
