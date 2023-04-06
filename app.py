8b710dda8df81bacbdc1a297a454be159ea5b2d0
Abel Carrizo <abelcarriizo@gmail.com>
2023-04-06 14:20:40 -0300

    configuracion bd

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-#Este archivo contiene la configuracion de la aplicacion
-
 from flask import Flask
 from routes.contacts import contacts
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+#Settings   
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://abel:123@localhost/contactsdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 app.register_blueprint(contacts)
