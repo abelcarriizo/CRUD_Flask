4fa4d31dbb0b8c1f1e9cff50fad737dbf11ed39d
Abel Carrizo <abelcarriizo@gmail.com>
2023-04-06 18:14:20 -0300

    añadiendo configuracion

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from routes.contacts import contacts
+from config import DATABASE_CONNECTION_URI
 
 app = Flask(__name__)
 
 #Settings   
 app.secret_key = 'secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://abel:123@localhost/contactsdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 app.register_blueprint(contacts)
