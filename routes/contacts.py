632e1db89d6d15abeaea052a3a7749f8e964515a
Abel Carrizo <abelcarriizo@outlook.com>
2024-01-01 22:45:48 -0300

    Añadido listar contactos

--- routes/contacts.py
+++ routes/contacts.py
@@ -6,7 +6,8 @@ contacts = Blueprint('contacts', __name__)
 
 @contacts.route("/")
 def home():
-    return render_template('index.html')
+    contacts = Contact.query.all()
+    return render_template('index.html', contacts=contacts)
 
 @contacts.route("/new", methods=["POST"])
 def add():
